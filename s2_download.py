05f9e20b0e45f4ab161da7a2d46a1175b2fef58c
Zerhigh <samuel@plix.at>
2025-04-09 18:09:52 +0200

    refactoring

--- s2_download.py
+++ s2_download.py
@@ -98,7 +98,6 @@ def resample_mask_torch(arr: np.ndarray, scale_factor: float) -> np.ndarray:
         #antialias=True
     ).squeeze().numpy()
     return (ret_arr.squeeze()).astype(bool)
-    #return (ret_arr.squeeze() > 0.5).astype(np.uint8)
 
 
 def resample_torch(arr: np.ndarray, scale_factor: float) -> np.ndarray:
@@ -281,64 +280,6 @@ def masked_match_histograms(image: np.ndarray,
     return matched.filled()
 
 
-def plot_band_histograms(array1, array2, array3, figname, corr, nodata_val=0, bins=256):
-    """
-    Plots histograms of each band for two multi-band arrays side-by-side.
-    - array1, array2: shape (bands, height, width)
-    - nodata_val: value to ignore (e.g., 0)
-    """
-    assert array1.shape == array2.shape, "Arrays must be the same shape"
-    bands = array1.shape[0]
-
-    fig, axs = plt.subplots(1, bands, figsize=(5 * bands, 4))
-
-    if bands == 1:
-        axs = [axs]  # ensure axs is iterable for single band
-
-    for b in range(bands):
-        a1 = array1[b].ravel()
-        a2 = array2[b].ravel()
-        a3 = array2[b].ravel()
-
-        a1 = a1[a1 != nodata_val]
-        a2 = a2[a2 != nodata_val]
-        a3 = a3[a3 != nodata_val]
-
-        hist1, bin_edges = np.histogram(a1, bins=bins, density=True)
-        hist2, _ = np.histogram(a2, bins=bin_edges, density=True)
-        hist3, _ = np.histogram(a3, bins=bin_edges, density=True)
-
-        bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2
-
-        axs[b].plot(bin_centers, hist1, label='lr harm', color='blue')
-        axs[b].plot(bin_centers, hist2, label='lr', color='orange')
-        axs[b].plot(bin_centers, hist3, label='hr', color='green')
-        axs[b].set_title(f'Band {b + 1}')
-        axs[b].set_xlabel('Pixel Value')
-        axs[b].set_ylabel('Density')
-        axs[b].legend()
-
-    plt.title(f'corr {corr}')
-
-    plt.tight_layout()
-    plt.savefig(f'fig_{figname}.png')
-
-
-def bandwise_corr(img1, img2, nodata_val=0):
-    correlations = []
-    for b in range(img1.shape[0]):
-        a = img1[b]
-        b_ = img2[b]
-        valid_mask = (a != nodata_val) & (b_ != nodata_val)
-
-        if np.any(valid_mask):
-            corr = np.corrcoef(a[valid_mask], b_[valid_mask])[0, 1]
-        else:
-            corr = np.nan
-        correlations.append(corr)
-    return correlations
-
-
 def to_squared_geopackage(data_: pd.DataFrame) -> gpd.GeoDataFrame:
     def to_point(row):
         return shapely.Point(row['lon'], row['lat'])
@@ -364,6 +305,7 @@ def to_squared_geopackage(data_: pd.DataFrame) -> gpd.GeoDataFrame:
 
 
 def _process_batch(data: Tuple[Hashable, pd.DataFrame],
+                   orthofoto_path: Path,
                    hr_compressed_mask_path: Path,
                    hr_orthofoto_path: Path,
                    hr_harm_path: Path,
@@ -372,12 +314,13 @@ def _process_batch(data: Tuple[Hashable, pd.DataFrame],
     index, batch = data
     batch_statistics = {}
     try:
+        ########### LOAD S2 TILES & ORTHOFOTOS ###########
         # first item should have same lat/lon as all others
         lat, lon, id = batch.iloc[0]['lat'], batch.iloc[0]['lon'], batch.iloc[0]['id']
         new_id = f"{id:05d}"
         matched_dataset = MatchedData(id=new_id)
 
-        # Prepare the raster transform
+        # Prepare the Sentinel-2 raster transform
         geotransform = cubexpress.lonlat2rt(
             lon=lon,
             lat=lat,
@@ -388,17 +331,17 @@ def _process_batch(data: Tuple[Hashable, pd.DataFrame],
         s2_hr_trafo = rasterio.transform.Affine(gt['scaleX'] / UPSAMPLE, gt['shearX'], gt['translateX'],
                                                 gt['shearY'], gt['scaleY'] / UPSAMPLE, gt['translateY'])
 
-        # download and load all sentinel images into memory (for all bands)
+        # download and load all sentinel tiles
         # download_sentinel2_samples(data=batch, geotransform=geotransform, output_path=BASE / 'tmp')
         batch_s2_paths = [Path(BASE / "tmp" / f"{row['s2_download_id']}.tif") for _, row in batch.iterrows()]
+
+        # load all s2 tiles into memory (for all bands)
         s2_profile, s2_data = load_sentinel2_samples(path_list=batch_s2_paths)
         s2_profile.update(blockxsize=32, blockysize=32, nodata=0)
 
         # load corresponding orthofoto
-        # ortho_base = Path('/data/USERS/shollend/orthophoto/austria_full_allclasses_regridded')
-        ortho_base = Path('C:/Users/PC/Coding/cubexpress_austria/local_experiment/local_orthofotos')
-        input_path = ortho_base / 'input' / f'input_{id}.tif'
-        target_path = ortho_base / 'target' / f'target_{id}.tif'
+        input_path = orthofoto_path / 'input' / f'input_{id}.tif'
+        target_path = orthofoto_path / 'target' / f'target_{id}.tif'
 
         # transform and resample the orthofoto images and masks
         mdata, mprofile = transform_ortho(ortho_fp=target_path,
@@ -415,15 +358,17 @@ def _process_batch(data: Tuple[Hashable, pd.DataFrame],
                                           s2_height=IMG_SHAPE[1],
                                           )
 
-        # All resampling will be conducted with Pytorch Pillow bilinear wrapper as it introduces the least errors
-        # https://zuru.tech/blog/the-dangers-behind-image-resizing
-        # methods for dimesion filtering is included in resampling_*torch()
+        ########### MATCH FOR S2-TILES ###########
 
-        # Binary masks for processing will be saved as nodata=True in accordance with np.masked array Regulation
-        # Binary masks saved and applied to images will be inversely saved: nodata = False to allow binary
+        # Binary masks for processing will be inverted to nodata=True in accordance with np.masked array Regulation
+        # Binary masks saved and applied to images will saved as: nodata = False to allow binary
         # multiplication: image (4, 128, 128) * mask (128, 128)
         nodata_hr_mdata = np.all(mdata != 0, axis=0)  # (512, 512)
         nodata_hr_odata = np.all(odata != 0, axis=0)  # (512, 512)
+
+        # All resampling will be conducted with Pytorch Pillow bilinear wrapper as it introduces the least errors
+        # https://zuru.tech/blog/the-dangers-behind-image-resizing
+        # methods for dimension filtering is included in resample_*-torch()
         nodata_lr_mdata = resample_mask_torch(arr=nodata_hr_mdata, scale_factor=0.25)  # (128, 128)
         nodata_lr_odata = resample_mask_torch(arr=nodata_hr_odata, scale_factor=0.25)  # (128, 128)
 
@@ -431,7 +376,7 @@ def _process_batch(data: Tuple[Hashable, pd.DataFrame],
         for s2_name, s2_image in s2_data.items():
             s2_tile = S2Batch(s2_id=s2_name)
 
-            # [4, 3, 2, 8] bands Required -> remapped to [3, 2, 1, 7] for R, G, B, NIR
+            # Sentinel-2 tile extraction: [4, 3, 2, 8] bands Required -> remapped to [3, 2, 1, 7] for R, G, B, NIR
             lr_s2_base = s2_image[[3, 2, 1, 7], :, :] / 10_000
 
             # create binary mask for filtering and homogenising all image data
@@ -441,10 +386,9 @@ def _process_batch(data: Tuple[Hashable, pd.DataFrame],
             nodata_lr = np.all(np.stack([nodata_lr_s2, nodata_lr_mdata, nodata_lr_odata], axis=0) != 0, axis=0)  # (128, 128)
             nodata_hr = resample_mask_torch(arr=nodata_lr, scale_factor=4)  # (512, 512)
 
-            # apply masks and normalize
+            # Normalize images and calculate histogram matching
             hr_ortho_norm = odata / 255
             lr_s2_norm = lr_s2_base
-
             hr_harm_self = masked_match_histograms(image=hr_ortho_norm,
                                                    reference=lr_s2_norm,
                                                    image_mask=~nodata_hr,
@@ -454,15 +398,13 @@ def _process_batch(data: Tuple[Hashable, pd.DataFrame],
             # reduce with bilinear interpolation -> (4, 128, 128)
             lr_harm_self = resample_torch(arr=hr_harm_self, scale_factor=0.25)  # (4, 128, 128)
 
-            # Compute block-wise correlation between LR and LRharm
-            # the way i have written the correlation HIGH nan value images are prioritized as they have a
-            # large overlap!!!!!!!! -> switched to block correlation again
+            # Compute block-wise correlation between LR and LRharm emitting NaN values
             corr = utils_histogram.fast_block_correlation(lr_s2_norm * nodata_lr_s2, lr_harm_self * nodata_lr,
                                                           none_value=0, block_size=16)
 
             # Report the 10th percentile of the correlation (low correlation) without nans
             low_corr = np.nanquantile(corr, 0.10)
-            #weighted_corr_coefficient = low_corr * (1 - np.sign(low_corr) * np.sum(nodata_lr == 0)/(128*128))
+            # weighted_corr_coefficient = low_corr * (1 - np.sign(low_corr) * np.sum(nodata_lr == 0)/(128*128))
 
             s2_tile.s2_corr = round(float(low_corr), 4)
             s2_tile.lr_harm = lr_harm_self
@@ -471,65 +413,20 @@ def _process_batch(data: Tuple[Hashable, pd.DataFrame],
             s2_tile.hr_nodata = nodata_hr
             s2_tile.s2_nodata = nodata_lr_s2
             s2_tile.median_corr = np.nanmedian(corr)
-            # introduce a filter to determine the nan amount in s2 and filter for it
             s2_tile.s2_nodata_perc = round(float(np.sum(nodata_lr_s2 == 0) / (128 * 128)), 5)
             s2_tile.total_nodata_perc = round(float(np.sum(nodata_lr == 0) / (128 * 128)), 5)
 
             matched_dataset.S2Batches[s2_tile.s2_id] = s2_tile
 
-            # plot_band_histograms(array1=lr_harm_self * nodata_lr, array2=lr_s2_norm * nodata_lr_s2,
-            #                      array3=hr_ortho_norm * nodata_hr,
-            #                      figname=s2_name,
-            #                      corr=[low_corr, weighted_corr])
-
-            # s2_corrs[s2_name] = round(float(low_corr), 4)
-            # lr_harms[s2_name] = lr_harm_self
-            # hr_harms[s2_name] = hr_harm_self
-            # lr_nodata[s2_name] = nodata_lr
-            # hr_nodata[s2_name] = nodata_hr
-            # s2_nodata[s2_name] = nodata_lr_s2
-            # median_corr[s2_name] = np.nanmedian(corr)
-            # # introduce a filter to determine the nan amount in s2 and filter for it
-            # s2_nodata_perc[s2_name] = round(float(np.sum(nodata_lr_s2 == 0) / (128 * 128)), 5)
-            # total_nodata_perc[s2_name] = round(float(np.sum(nodata_lr == 0) / (128 * 128)), 5)
-            #weighted_corr[s2_name] = round(float(weighted_corr_coefficient), 4)
-
-        # s2_merged = {k: (total_nodata_perc[k], s2_corrs[k]) for k in total_nodata_perc if k in s2_corrs}
-        # s2_merged_filtered = {k: v for k, v in s2_merged.items() if None not in v}
-        # s2_merged_filtered_sorted = dict(sorted(s2_merged_filtered.items(), key=lambda item: (1-item[1][0], item[1][1]), reverse=True))
-        # best_s2_key = next(iter(s2_merged_filtered_sorted)) if len(s2_merged_filtered_sorted) > 0 else None
-        # if best_s2_key is None:
-        #     # select the onE with the lowest sentinel2 nodata values
-        #     lowest_s2_key = min(s2_nodata_perc, key=s2_nodata_perc.get)
-        #
-        #     batch_statistics['s2_available'] = False
-        #     batch_statistics['nodata_total'] = round(np.sum(hr_nodata[lowest_s2_key] == 0) / (IMG_SHAPE[0] * IMG_SHAPE[1]), 5)
-        #     batch_statistics['nodata_ortho'] = round(np.sum(nodata_hr_odata == 0) / (IMG_SHAPE[0] * IMG_SHAPE[1]), 5)
-        #     batch_statistics['nodata_mask'] = round(np.sum(nodata_hr_mdata == 0) / (IMG_SHAPE[0] * IMG_SHAPE[1]), 5)
-        #     batch_statistics['nodata_s2'] = round(np.sum(s2_nodata[lowest_s2_key] == 0) / (128*128), 5)
-        #     batch_statistics['contains_nodata'] = True
-        #
-        #     none_cols = ['low_corr', 'lr_harm_path', 'lr_s2_path', 'hr_harm_path', 'hr_othofoto_path', 'hr_mask_path',
-        #                  'median_corr', 'all_corrs', 'all_nodata_perc',]
-        #
-        #     for col in none_cols:
-        #         batch_statistics[col] = None
-        #
-        #     selected_row = batch.loc[batch["s2_download_id"] == lowest_s2_key]
-        #     row_dict = selected_row.iloc[0].to_dict()
-        #     bad_substrings = ['Unnamed', 's2_available']
-        #     for k, v in row_dict.items():
-        #         if not any(sub in k for sub in bad_substrings):
-        #             batch_statistics[k] = v
-
-        # apply shared mask
-
         # filter and set the best suited tile
         matched_dataset.set_best_tile()
+
+        # apply shared masks
         masked_mdata = mdata * matched_dataset.best_tile.hr_nodata
         masked_odata = odata * matched_dataset.best_tile.hr_nodata
 
         ########### ADD METADATA ###########
+
         batch_statistics['low_corr'] = matched_dataset.best_tile.s2_corr
         batch_statistics['all_corrs'] = {tile.s2_id: tile.s2_corr for tile in matched_dataset.S2Batches.values()}
         batch_statistics['all_nodata_perc'] = {tile.s2_id: tile.total_nodata_perc for tile in matched_dataset.S2Batches.values()}
@@ -553,13 +450,14 @@ def _process_batch(data: Tuple[Hashable, pd.DataFrame],
         for k, v in mask_stats.items():
             batch_statistics[k] = v
 
-        # derive nodata value on the nodata distribution value from the distribution, rounded to 5 decimals
+        # add nodata flag based on the total no data count aggregated over all masks
         if batch_statistics['nodata_total'] > 0:
             batch_statistics['contains_nodata'] = True
         else:
             batch_statistics['contains_nodata'] = False
 
         ########### WRITE FILES ###########
+
         batch_statistics['hr_mask_path'] = hr_compressed_mask_path / f'HR_mask_{new_id}.tif'
         batch_statistics['hr_othofoto_path'] = hr_orthofoto_path / f'HR_ortho_{new_id}.tif'
         batch_statistics['hr_harm_path'] = hr_harm_path / f'HR_ortho_{new_id}.tif'
@@ -600,11 +498,9 @@ def _process_batch(data: Tuple[Hashable, pd.DataFrame],
 
         with rasterio.open(hr_harm_path / f'HR_ortho_{new_id}.tif', "w", **harm_hr_profile) as dst:
             dst.write((matched_dataset.best_tile.hr_harm * matched_dataset.best_tile.hr_nodata * 10_000).round().astype(rasterio.uint16))
-            #dst.write((hr_harms[best_s2_key] * hr_nodata[best_s2_key] * 10_000).round().astype(rasterio.uint16))
 
         # Normal S2
         with rasterio.open(lr_s2_path / f'S2_{new_id}.tif', "w", **s2_profile) as dst:
-            # lr_save_data = s2_data[best_s2_key] * lr_nodata[best_s2_key]
             dst.write(s2_data[matched_dataset.best_tile.s2_id] * matched_dataset.best_tile.lr_nodata)
 
         # Harmonized S2
@@ -621,7 +517,6 @@ def _process_batch(data: Tuple[Hashable, pd.DataFrame],
 
         with rasterio.open(lr_harm_path / f'S2_{new_id}.tif', "w", **harm_lr_profile) as dst:
             dst.write((matched_dataset.best_tile.lr_harm * matched_dataset.best_tile.lr_nodata * 10_000).round().astype(rasterio.uint16))
-            #dst.write((lr_harms[best_s2_key] * lr_nodata[best_s2_key] * 10_000).round().astype(rasterio.uint16))
 
         # remove unwanted sentinel images
         # for i, row in batch.iterrows():
@@ -647,13 +542,17 @@ if __name__ == '__main__':
         #df_filtered: pd.DataFrame = pd.read_csv(BASE / "sample_s2_wmeta.csv")
 
     # ortho path
-    # /data/USERS/shollend/orthophoto/austria_full_allclasses_regridded
+    # inp_ortho_path = Path('/data/USERS/shollend/orthophoto/austria_full_allclasses_regridded')
+    # inp_ortho_path = Path('C:/Users/PC/Coding/cubexpress_austria/local_experiment/local_orthofotos')
+
     # table path
     # /home/shollend/shares/users/master/metadata/cubexpress/merged_ALL_S2_filter.csv
+
     # base
     # /data/USERS/shollend/combined_download
 
     # generate folders
+    inp_ortho_path = Path('C:/Users/PC/Coding/cubexpress_austria/local_experiment/local_orthofotos')
     out_ortho_target = BASE / 'output' / 'hr_mask'
     out_ortho_input = BASE / 'output' / 'hr_orthofoto'
     out_ortho_input_harm = BASE / 'output' / 'hr_harm_orthofoto'
@@ -668,7 +567,7 @@ if __name__ == '__main__':
     out_sentinel2_harm.mkdir(parents=True, exist_ok=True)
     tmp.mkdir(parents=True, exist_ok=True)
 
-    # Download with cubexpress
+    # Aggregate S2-tiles to corresponding orthofoto
     df_batches = df_filtered.groupby("id")
 
     print('downloading sentinel2')
@@ -677,31 +576,34 @@ if __name__ == '__main__':
     # Create a multiprocessing list of tasks
     rows = [(idx, batch) for idx, batch in df_batches]
 
-    res = []
-    for row in tqdm(rows):
-        res.append(_process_batch(data=row,
-                                  hr_compressed_mask_path=out_ortho_target,
-                                  hr_orthofoto_path=out_ortho_input,
-                                  hr_harm_path=out_ortho_input_harm,
-                                  lr_s2_path=out_sentinel2,
-                                  lr_harm_path=out_sentinel2_harm,
-                                  ))
+    # res = []
+    # for row in tqdm(rows):
+    #     res.append(_process_batch(data=row,
+    #                               orthofoto_path=inp_ortho_path,
+    #                               hr_compressed_mask_path=out_ortho_target,
+    #                               hr_orthofoto_path=out_ortho_input,
+    #                               hr_harm_path=out_ortho_input_harm,
+    #                               lr_s2_path=out_sentinel2,
+    #                               lr_harm_path=out_sentinel2_harm,
+    #                               ))
 
-    # _process_batch_partial = partial(_process_batch,
-    #                                  hr_compressed_mask_path=out_ortho_target,
-    #                                  hr_orthofoto_path=out_ortho_input,
-    #                                  hr_harm_path=out_ortho_input_harm,
-    #                                  lr_s2_path=out_sentinel2,
-    #                                  lr_harm_path=out_sentinel2_harm,)
-    #
-    # # Run parallel processing
-    # with ProcessPoolExecutor(max_workers=os.cpu_count()) as executor:
-    #     res = list(tqdm(executor.map(_process_batch_partial, rows[:151]), total=len(rows)))
+    _process_batch_partial = partial(_process_batch,
+                                     orthofoto_path = inp_ortho_path,
+                                     hr_compressed_mask_path=out_ortho_target,
+                                     hr_orthofoto_path=out_ortho_input,
+                                     hr_harm_path=out_ortho_input_harm,
+                                     lr_s2_path=out_sentinel2,
+                                     lr_harm_path=out_sentinel2_harm,)
+
+    # Run parallel processing
+    with ProcessPoolExecutor(max_workers=os.cpu_count()) as executor:
+        res = list(tqdm(executor.map(_process_batch_partial, rows), total=len(rows)))
 
     out_df = pd.DataFrame.from_records(res)
-    out_gdf = to_squared_geopackage(data_=out_df)
     out_df.to_csv(BASE / 's2_ortho_download_data.csv')
-    out_gdf.to_file(BASE / 's2_ortho_download_data.gpkg', driver='GPKG')
+
+    out_gdf = to_squared_geopackage(data_=out_df)
+    out_gdf.to_file(str(BASE / 's2_ortho_download_data.gpkg'), driver='GPKG')
 
     tstop = time.time()
     print(f'Script took {round(tstop - tstart, 2)}s')
